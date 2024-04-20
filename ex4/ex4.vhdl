e878dfcb3c27cd349364e6b2c540fd4532213e18
Filip Filmar <fmil@google.com>
2024-04-19 22:59:06 -0700

    feat: adds a hierarchical library import.

---
+++ ex4/ex4.vhdl
@@ -0,0 +1,8 @@
+library ex4_lib1;
+use ex4_lib1.lib1;
+
+package ex4 is
+
+    constant ex4_const: integer := lib1.c;
+
+end package ex4;
---
+++ ex4/lib1/lib1.vhdl
@@ -0,0 +1,8 @@
+-- The library here is ex4_lib1.
+
+package lib1 is
+    
+   -- This is ex4_lib1.lib1.c
+   constant c: integer := 1; 
+    
+end package lib1;
